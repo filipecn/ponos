13370701ec34aa1926078a73d10d4db984d57b6e
filipecn <fuiripecn@gmail.com>
2019-12-23 15:58:18 -0300

    install glfw

--- ext/stb.cmake
+++ ext/stb.cmake
@@ -1,1 +1,6 @@
 set(STB_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/ext/stb")
+
+install(FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/ext/stb/stb_image.h
+        ${CMAKE_CURRENT_SOURCE_DIR}/ext/stb/stb_truetype.h
+        DESTINATION ${INSTALL_PATH}/include)
