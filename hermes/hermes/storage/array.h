7f2a2b9c6330b7e850eb2beaaa20c865d3de2102
filipecn <fuiripecn@gmail.com>
2020-03-01 15:55:37 -0300

    fix hermes array include

--- hermes/hermes/storage/array.h
+++ hermes/hermes/storage/array.h
@@ -29,8 +29,10 @@
 #define HERMES_STORAGE_ARRAY_H
 
 #include <hermes/common/cuda.h>
+#include <hermes/common/cuda_parallel.h>
 #include <hermes/common/index.h>
 #include <ponos/storage/array.h>
+
 
 namespace hermes {
 
